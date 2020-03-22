14cb16651590a86008d6b7f69e965a9a6ffa9441
Илья Макаров <i.o.makarov@2gis.ru>
2020-03-22 12:53:12 +0700

    tmp-2

--- projects/Core/IdxUtils.h
+++ projects/Core/IdxUtils.h
@@ -12,9 +12,9 @@ auto apply_operation(
 	const Grid1D & rhs,
 	const std::function<Precision(Precision, Precision)> & function);
 
-auto apply_conv_factor(const Grid1D & input);
+auto apply_conv_factor(const Grid1D & input, Precision mult);
 
-auto apply_corr_factor(const Grid1D & input);
+auto apply_corr_factor(const Grid1D & input, Precision mult);
 
 } // namespace utils
 
