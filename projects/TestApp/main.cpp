150ad52b378a9ebeedc8ff16db6d7da93d5d1a7e
Ilya Makarov <i.o.makarov@2gis.ru>
2020-04-07 13:46:25 +0700

    Added additional plots

--- projects/TestApp/main.cpp
+++ projects/TestApp/main.cpp
@@ -237,7 +237,7 @@ int main() try
 		std::swap(values_1, values_2);
 
 		//! draw values_1;
-		std::vector<double> x, y;
+		/*std::vector<double> x, y;
 		for(auto x_idx = 0u; x_idx < g_z_grid_size; ++x_idx)
 		{
 			x.push_back(static_cast<Precision>(x_idx) * g_z_grid_step);
@@ -247,30 +247,42 @@ int main() try
 		}
 		namespace plt = matplotlibcpp;
 		plt::plot(x, y);
-		plt::show();
+		plt::show();*/
 
-		/*std::vector<std::vector<double>> x, y, z;
+		std::vector<double> x, y;
 		for(auto x_idx = 0u; x_idx < g_z_grid_size; ++x_idx)
 		{
-			std::vector<double> x_row, y_row, z_row;
-			for(auto z_idx = 0u; z_idx < g_z_grid_size; ++z_idx)
-			{
-				x_row.push_back(x_idx * g_z_grid_step);
-				y_row.push_back(z_idx * g_z_grid_step);
-				const auto f_value = u_func(values_1.u, x_idx, z_idx);
-				z_row.push_back(f_value);
-			}
-			x.push_back(x_row);
-			y.push_back(y_row);
-			z.push_back(z_row);
+			x.push_back(static_cast<Precision>(x_idx) * g_z_grid_step);
+			constexpr auto z_0 = g_z_grid_size / 2;
+			const auto f_value = u_func(values_1.u, z_0, x_idx);
+			y.push_back(f_value);
 		}
-
 		namespace plt = matplotlibcpp;
-		
-		plt::plot_surface(x, y, z);
-		plt::xlabel("x");
-		plt::ylabel("z");
-		plt::show();*/
+		plt::plot(x, y);
+		plt::show();
+
+		/*{
+			std::vector<std::vector<double>> x, y, z;
+			for (auto x_idx = 0u; x_idx < g_z_grid_size; ++x_idx) {
+				std::vector<double> x_row, y_row, z_row;
+				for (auto z_idx = 0u; z_idx < g_z_grid_size; ++z_idx) {
+					x_row.push_back(x_idx * g_z_grid_step);
+					y_row.push_back(z_idx * g_z_grid_step);
+					const auto f_value = u_func(values_1.u, x_idx, z_idx);
+					z_row.push_back(f_value);
+				}
+				x.push_back(x_row);
+				y.push_back(y_row);
+				z.push_back(z_row);
+			}
+
+			namespace plt = matplotlibcpp;
+
+			plt::plot_surface(x, y, z);
+			plt::xlabel("x");
+			plt::ylabel("z");
+			plt::show();
+		}*/
 	}
 }
 catch(const utils::MKLException & exception)
