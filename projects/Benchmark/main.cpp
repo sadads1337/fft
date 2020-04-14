124c5283b4da94d0e4fe03f3b797c561cb9f1443
Ilya Makarov <i.o.makarov@2gis.ru>
2020-04-14 12:52:17 +0700

    TimeGuard moved to Core lib

--- projects/Benchmark/main.cpp
+++ projects/Benchmark/main.cpp
@@ -1,34 +1,11 @@
 #include <Scheme/Scheme.h>
 
+#include <Core/TimeGuard.h>
 #include <Core/Types.h>
 #include <Core/MKL/Utils.h>
 
 #include <iostream>
 #include <chrono>
-
-namespace
-{
-
-class TimeGuard
-{
-public:
-	TimeGuard()
-		: start_{std::chrono::high_resolution_clock::now()}
-	{
-	}
-
-	~TimeGuard()
-	{
-		const auto stop = std::chrono::high_resolution_clock::now();
-		const auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(stop - start_);
-		std::cout << "Elapsed in " << elapsed.count() << "seconds";
-	}
-
-private:
-	std::chrono::time_point<std::chrono::high_resolution_clock> start_;
-};
-
-} // namespace
 
 int main() try
 {
@@ -47,7 +24,7 @@ int main() try
 	};
 
 	{
-		TimeGuard time_guard{};
+		utils::TimeGuard time_guard{};
 		for (auto t_idx = 0u; t_idx < scheme::g_t_grid_size; ++t_idx) {
 			calculate_one_step(values_1, values_2, env, t_idx);
 
---
+++ projects/Core/TimeGuard.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <chrono>
+
+namespace utils
+{
+
+class TimeGuard
+{
+public:
+	TimeGuard();
+	~TimeGuard();
+
+private:
+	std::chrono::time_point<std::chrono::high_resolution_clock> start_;
+};
+
+} // namespace utils
