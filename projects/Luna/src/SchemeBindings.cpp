4bd3a745face61c7b93bc2ca9a7cfedefa358611
Илья Макаров <i.o.makarov@2gis.ru>
2020-05-15 21:35:57 +0700

    Added luna bindings for scheme module

--- projects/Luna/src/SchemeBindings.cpp
+++ projects/Luna/src/SchemeBindings.cpp
@@ -1,58 +1,191 @@
+#include <Core/MoveOnly.h>
+#include <Core/Types.h>
 #include <Luna/SchemeBindings.h>
 
-#include <cstdint>
 #include <cassert>
 #include <iostream>
 
-void init_vector(
-    const std::int32_t fg_num,
-    const std::int32_t fg_size,
-    luna::ucenv::OutputDF & vec)
+namespace {
+
+inline auto calculate_source()
 {
-  vec.create(fg_size * sizeof(std::int32_t));
-  auto * const vec_array = vec.getData<std::int32_t>();
-  for (auto idx = 0; idx < fg_size; ++idx)
-  {
-    vec_array[idx] = fg_num;
-  }
+  return scheme::source(4, static_cast<Precision>(1.), scheme::g_t_grid_step,
+                 scheme::g_z_grid_step, scheme::g_t_grid_size);
 }
 
-void sum_vectors(
-    const luna::ucenv::InputDF & x,
-    const luna::ucenv::InputDF & y,
-    const std::int32_t fg_size,
-    luna::ucenv::OutputDF & result)
+inline auto operator==(const scheme::Values & lhs, const scheme::Values & rhs) noexcept
 {
-  assert(x.getArraySize<std::int32_t>() == y.getArraySize<std::int32_t>());
-  assert(static_cast<std::size_t>(fg_size) == x.getArraySize<std::int32_t>());
-
-  result.setArray(fg_size, std::int32_t{ 0 });
-  result.create(fg_size * sizeof(std::int32_t));
-  auto * const result_array = result.getData<std::int32_t>();
-  const auto * const x_array = x.getData<std::int32_t>();
-  const auto * const y_array = y.getData<std::int32_t>();
-
-  for (auto idx = 0; idx < fg_size; ++idx)
-  {
-    result_array[idx] = x_array[idx] + y_array[idx];
-  }
-
+  return lhs.u == rhs.u
+         && lhs.w == rhs.w
+         && lhs.p == rhs.p
+         && lhs.q == rhs.q
+         && lhs.s == rhs.s;
 }
 
-void check_vector(
-    const luna::ucenv::InputDF & x,
-    const std::int32_t fg_num,
-    const std::int32_t fg_size)
+inline auto operator!=(const scheme::Values & lhs, const scheme::Values & rhs) noexcept
 {
-  assert(static_cast<std::size_t>(fg_size) == x.getArraySize<std::int32_t>());
+  return !(lhs == rhs);
+}
 
-  const auto * const x_array = x.getData<std::int32_t>();
-  const auto correct_value = static_cast<std::int32_t>(fg_num * 2);
-  for (auto idx = 0; idx < fg_size; ++idx)
-  {
-    if (correct_value != x_array[idx])
+template<typename F, typename... T>
+decltype(auto) no_omp_wrapper(F && function, T && ...args)
+{
+#if FFT_ENABLE_OPENMP
+#error "OPENMP must be disabled while running with LuNA RTS"
+#endif
+  return std::forward<F>(function)(std::forward<T>(args)...);
+}
+
+} // namespace
+
+void init_env(
+    [[maybe_unused]] const std::int32_t fg_num,
+    [[maybe_unused]] const std::int32_t fg_size,
+    luna::ucenv::OutputDF & out_env)
+{
+  //! Since our environment is constant \b fg_num and \b fg_size unused.
+  //! \todo: Fix it later, when environment become non constant.
+
+  const scheme::Env env{
+      Grid1D(scheme::g_k_limit, static_cast<Precision>(1.)),
+      Grid1D(scheme::g_k_limit, static_cast<Precision>(1.)),
+      Grid1D(scheme::g_k_limit, static_cast<Precision>(1.)),
+      utils::move_only(calculate_source()),
+  };
+
+  out_env.set(env);
+}
+
+void check_env(
+    const luna::ucenv::InputDF & env,
+    [[maybe_unused]] const std::int32_t fg_num,
+    [[maybe_unused]] const std::int32_t fg_size)
+{
+  const auto env_data = env.getData<scheme::Env>();
+  assert(env_data);
+
+  const auto check_values = [](const auto & values, const auto & expected_value) {
+    for (const auto & value : values)
     {
-      std::cout << "failed in idx: " << idx;
+      if (expected_value != value)
+      {
+        std::cout << "Failed check in: " << typeid(decltype(values)).name();
+      }
     }
+  };
+
+  check_values(env_data->rho, static_cast<Precision>(1.));
+  check_values(env_data->lambda, static_cast<Precision>(1.));
+  check_values(env_data->mu, static_cast<Precision>(1.));
+
+  if (env_data->f != utils::move_only(calculate_source()))
+  {
+    std::cout << std::string{__func__} + " failed check in: "
+              << typeid(env_data->f).name()
+              << std::endl;
+  }
+}
+
+void init_values(
+    [[maybe_unused]] const std::int32_t fg_num,
+    [[maybe_unused]] const std::int32_t fg_size,
+    luna::ucenv::OutputDF & values)
+{
+  //! Zero initialized scheme::Values
+  //! Since our fragment is only one \b fg_num and \b fg_size unused.
+  //! \todo: Fix it later, when fragment number become > 1.
+
+  values.set(scheme::Values{});
+}
+
+//! import check_prev_values(value #prev_values, int #fgnum, int #size) as check_prev_values;
+//! import check_values(value #values, int #fgnum, int #size) as check_values;
+//! \note scheme::g_z_grid_size = fg_num * fg_size;
+//! \warning only for debug purposes;
+void check_values(
+    const luna::ucenv::InputDF & values,
+    [[maybe_unused]] const std::int32_t fg_num,
+    [[maybe_unused]] const std::int32_t fg_size)
+{
+  //! Zero initialized scheme::Values
+  //! Since our fragment is only one \b fg_num and \b fg_size unused.
+  //! \todo: Fix it later, when fragment number become > 1.
+
+  const auto values_data = values.getData<scheme::Values>();
+  assert(values_data);
+
+  const auto check_values = [](const auto & values, const auto & expected_value) {
+    for (const auto & value : values)
+    {
+      for (const auto & el : value)
+      {
+        if (expected_value != el)
+        {
+          std::cout << std::string{__func__} + " failed check in: "
+                    << typeid(decltype(values)).name()
+                    << std::endl;
+        }
+      }
+    }
+  };
+
+  check_values(values_data->u, static_cast<Precision>(0.));
+  check_values(values_data->w, static_cast<Precision>(0.));
+  check_values(values_data->p, static_cast<Precision>(0.));
+  check_values(values_data->q, static_cast<Precision>(0.));
+  check_values(values_data->s, static_cast<Precision>(0.));
+}
+
+void calculate_one_step(
+    const luna::ucenv::InputDF & prev_values,
+    const luna::ucenv::InputDF & env,
+    const std::int32_t t_idx,
+    luna::ucenv::OutputDF & values)
+{
+  const auto prev_values_data = prev_values.getData<scheme::Values>();
+  assert(prev_values_data);
+  const auto env_data = env.getData<scheme::Env>();
+  assert(env_data);
+
+  //! \todo: fix redudant copy
+  scheme::Values values_data{};
+
+  no_omp_wrapper(scheme::calculate_one_step, *prev_values_data, values_data, *env_data, t_idx);
+
+  assert(values_data != *prev_values_data);
+
+  values.set(values_data);
+}
+
+void check_one_step(
+    const luna::ucenv::InputDF & prev_values,
+    const luna::ucenv::InputDF & values,
+    const luna::ucenv::InputDF & env,
+    const std::int32_t t_idx,
+    [[maybe_unused]] const std::int32_t fg_num,
+    [[maybe_unused]] const std::int32_t fg_size)
+{
+  //! Zero initialized scheme::Values
+  //! Since our fragment is only one \b fg_num and \b fg_size unused.
+  //! \todo: Fix it later, when fragment number become > 1.
+
+  scheme::Values expected_values_data{};
+
+  const auto prev_values_data = prev_values.getData<scheme::Values>();
+  assert(prev_values_data);
+  const auto values_data = values.getData<scheme::Values>();
+  assert(values_data);
+  const auto env_data = env.getData<scheme::Env>();
+  assert(env_data);
+
+  //! Since \b prev_values are correct, we check only next step. Induction works here.
+  no_omp_wrapper(scheme::calculate_one_step, *prev_values_data, expected_values_data, *env_data, t_idx);
+
+  if (*values_data != expected_values_data)
+  {
+    std::cout << std::string{__func__} + " failed check in: "
+              << typeid(decltype(values)).name()
+              << " step: " << t_idx
+              << std::endl;
   }
 }
