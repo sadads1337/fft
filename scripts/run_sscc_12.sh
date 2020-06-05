be9a39e0e66e53817a1391d4aadf004303301e68
Ilya O. Makarov <makarov_i_o@login.sscc>
2020-06-06 00:19:51 +0700

    scripts

---
+++ scripts/run_sscc_12.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module load intel/2019/compilers
+module load intel/2019/lib/mkl
+module load intel/2019/lib/tbb
+module load intel/2019/mpi
+
+#SBATCH --nodes=1
+#SBATCH --threads-per-core=1
+#SBATCH --time=30:0
+#SBATCH --job-name=fft_makarov_12
+#SBATCH --partition=debug
+#SBATCH --qos=debug
+#SBATCH --ntasks-per-node=1
+#SBATCH --output="fft_makarov_test.txt"
+
+/home/mon.nsu/makarov_i_o/develop/fft/build/projects/Benchmark/Benchmark 12 5.115 65536 1024 7.62939e-06 32
